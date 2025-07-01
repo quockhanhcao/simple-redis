247cece455fffdbe17004a87a148d1e986fdc9c1
quockhanhcao <quockhanhcao99@gmail.com>
2025-07-02 06:25:50 +0700

    Initial set up, create tcp server and answer request with PONG

---
+++ app/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	defer listener.Close()
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	handleConnection(conn)
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+	fmt.Println("Received data: ", string(buf[:n]))
+	conn.Write([]byte("+PONG\r\n"))
+}
